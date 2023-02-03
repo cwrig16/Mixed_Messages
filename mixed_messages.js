a7cb093f9f6d6dfbaa38bf2583c47570ded6e2d2
Charlie Wright <wright.charlie1001@gmail.com>
2023-02-03 14:22:38 -0600

    add class, constructor, and getter for name

---
+++ mixed_messages.js
@@ -0,0 +1,14 @@
+class Morning {
+    constructor(name) {
+        this._name = name;
+        this._hello = [];
+        this._fun_fact = [];
+        this._goodbye = [];
+    }
+
+    get name() {
+        return this._name;
+    }
+
+
+}
